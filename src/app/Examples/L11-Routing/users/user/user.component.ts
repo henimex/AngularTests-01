5d3e40bd1f7ff457190945065744fdff4ea223f2
henimex <henimex@gmail.com>
2021-11-20 00:57:23 +0300

    Routing 101-2

--- src/app/Examples/L11-Routing/users/user/user.component.ts
+++ src/app/Examples/L11-Routing/users/user/user.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+export class UserComponent implements OnInit, OnDestroy {
+  user: { id: number, name: string };
+  paramsSubs : Subscription;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.checkParams();
+  }
+
+  checkParamsAlt() {
+    this.user = {
+      id: this.activatedRoute.snapshot.params['id'],
+      name: this.activatedRoute.snapshot.params['name']
+    };
+  }
+
+  checkParams() {
+    this.checkParamsAlt()
+    this.activatedRoute.params.subscribe(
+      params => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    //this is already handling by Angular but its for learning the kitchen.
+    //this.paramsSubs.unsubscribe();
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import {RouterModule, Routes} from '@angular/router';
 import {UsersComponent} from './Examples/L11-Routing/users/users.component';
 import {HomeComponent} from './Examples/L11-Routing/home/home.component';
 import {ServersComponent} from './Examples/L11-Routing/servers/servers.component';
+import {UserComponent} from './Examples/L11-Routing/users/user/user.component';
+import {EditServerComponent} from './Examples/L11-Routing/servers/edit-server/edit-server.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'servers', component: ServersComponent},
+  {path: 'servers/:id/edit', component: EditServerComponent},
   {path: 'users', component: UsersComponent},
+  {path: 'users/:id/:name', component: UserComponent},
 ];
 
 @NgModule({
